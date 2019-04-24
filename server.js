9a2aa9bf3e9748ec63c886ce476a334b1f16ec0f
idrak <playbox8g@gmail.com>
2019-04-25 00:04:01 +0800

    Cors and error handling

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const app = express();
 var port = process.env.PORT || 3100;
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	if (req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-	}
-	next();
-});
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR");
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});  
 
 app.use(bodyParser.json());
 
