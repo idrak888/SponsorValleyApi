ca4f17065c85fc37dc09cd2f63cd27643141506a
idrak <playbox8g@gmail.com>
2019-04-25 00:36:54 +0800

    ID handling

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers",
     "Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR");
-  if (req.headers.origin) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-  }
-  if (req.method === 'OPTIONS') {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).json({});
-  }
   next();
 });  
 
